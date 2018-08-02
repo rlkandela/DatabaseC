2374481c000cb907193ec8bc335efa64d44f4c30
Raul Candela Arias <rlkandela@ieee.org>
2018-08-02 19:13:37 +0200

    added the defines for the DB commands to be used in a switch

--- DBDefines.h
+++ DBDefines.h
@@ -1,4 +1,26 @@
 #ifndef __DBDEFINITIONS__
 #define __DBDEFINITIONS__
 #define LIST_SEPARATOR (char)248
+
+#define QUIT 0
+#define NEWDB 1
+#define SAVEDB 2
+#define LISTDB 3
+#define ACTIVEDB 4
+#define NEWCAB 5
+#define LISTCAB 6
+#define ACTIVECAB 7
+#define SET 8
+#define GET 9
+#define DEL 10
+#define RPUSH 11
+#define LPUSH 12
+#define RPOP 13
+#define LPOP 14
+#define RANGE 15
+#define KEY 16
+#define INC 17
+#define DEC 18
+#define NAN -1
+
 #endif
