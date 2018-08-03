fa628d57eb0eed126d52110bc750198c5269c5b0
Raul Candela Arias <rlkandela@ieee.org>
2018-08-03 19:59:06 +0200

    added the header of a constructor, a destructor, and list-value management

--- Node.h
+++ Node.h
@@ -1,10 +1,18 @@
 #ifndef __NODE__
 #define __NODE__
 #include <stdbool.h>
+
   typedef struct Node{
-      char *key;
-      char *val;
+      char* key;
+      char* val;
       bool isList;
       struct Node *next;
   }Node;
+
+  Node* createNode();
+  void deleteNode(Node*);
+  void rpush(char*,Node*);
+  void lpush(char*,Node*);
+  void rpop(Node*);
+  void lpop(Node*);
 #endif
