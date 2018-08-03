47f15f503e2d7366139946168100a6dc363f43e3
Raul Candela Arias <rlkandela@ieee.org>
2018-08-03 05:54:41 +0200

    added a reference in the header of a function

--- lecture.h
+++ lecture.h
@@ -2,7 +2,7 @@
 #define __LECTURE__
 #include "parameters.h"
 #include "DBDefines.h"
-    int ProcessInput(char *,char *,Parameters*);
+    int ProcessInput(char*,char**,Parameters*);
     int ObtainParameters(char*,Parameters*);
     void ProcessWord(char*, char**);
 #endif
