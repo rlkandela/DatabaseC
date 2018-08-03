926ea65a0f9d19d0d6f781d03d077a9e5ff22489
Raul Candela Arias <rlkandela@ieee.org>
2018-08-03 20:07:00 +0200

    added an include and also the createNode function that returns a node with the necesary memory allocated and initialized

--- Node.c
+++ Node.c
@@ -1,4 +1,13 @@
 #include "Node.h"
+#include "DBDefines.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+Node *createNode(void){
+  Node* ret = new(Node,1);
+  ret->key = nullptr;
+  ret->val = nullptr;
+  ret->next = nullptr;
+  ret->isList = false;
+}
