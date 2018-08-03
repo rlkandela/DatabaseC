32d507ed5222531f49357d46d732ccfbadd8c294
Raul Candela Arias <rlkandela@ieee.org>
2018-08-03 20:27:00 +0200

    added the functin rpush

--- Node.c
+++ Node.c
@@ -17,3 +17,18 @@ void deleteNode(Node* n){
   delete(n->key);
   delete(n);
 }
+
+void rpush(char* toAdd,Node* n){
+  n->isList = true;
+  char aux [1000];
+  strcpy(aux,n->val);
+  int len = strlen(n->val);
+  aux[len] = LIST_SEPARATOR;
+  aux[len+1] = 0;
+  strcat(aux,toAdd);
+  len = strlen(n->val);
+  delete(n->val);
+  n->val = new(char,len+1);
+  strcpy(n->val,aux);
+  return;
+}
