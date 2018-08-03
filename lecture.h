a6ce7153298d422fcc40d0ace8ce286ecf01e7ee
Raul Candela Arias <rlkandela@ieee.org>
2018-08-03 05:35:49 +0200

    added the header of a function that separates the input from a char* to a char* function and a Parameters struct

--- lecture.h
+++ lecture.h
@@ -2,6 +2,7 @@
 #define __LECTURE__
 #include "parameters.h"
 #include "DBDefines.h"
+    int ProcesarEntrada(char *,char *,Parametros*);
     int ObtainParameters(char*,Parameters*);
     void ProcessWord(char*, char**);
 #endif
