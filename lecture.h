26561467dd351510f8f98fa123110f492607b999
Raul Candela Arias <rlkandela@ieee.org>
2018-08-03 06:29:57 +0200

    added the header of a function that recives a command and clasifies it

--- lecture.h
+++ lecture.h
@@ -2,6 +2,7 @@
 #define __LECTURE__
 #include "parameters.h"
 #include "DBDefines.h"
+    int askForCommand(Parameters*);
     int ProcessInput(char*,char**,Parameters*);
     int ObtainParameters(char*,Parameters*);
     void ProcessWord(char*, char**);
