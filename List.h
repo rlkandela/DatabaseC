464f0ba25ef4730499b24eccee593fa05be07d6d
Raul Candela Arias <rlkandela@ieee.org>
2018-08-04 20:16:05 +0200

    added the header of the functions that will be implemented by the moment

--- List.h
+++ List.h
@@ -1,6 +1,7 @@
 #ifndef __LIST_STRUCT__
 #define __LIST_STRUCT__
 #include "Node.h"
+#include <stdbool.h>
 
 typedef struct List{
   char* name;
@@ -8,4 +9,10 @@ typedef struct List{
   int size;
 }List;
 
+List* createList(void);
+void deleteList(List*);
+Node* heap(List*);
+Node* searchKey(List*, char*);
+Node* previousNode(List*, Node*);
+bool quitNode(List*, Node*);
 #endif
