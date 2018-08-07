2a8b9e182babb08958ae1ab178ecebe11f6c8d26
Raul Candela Arias <rlkandela@ieee.org>
2018-08-07 22:17:05 +0200

    solved some errors so the project compiles and added some headers of some functions for the database commands

--- Database.h
+++ Database.h
@@ -2,6 +2,8 @@
 #define __DATABASE__
 #include <stdbool.h>
 #include "List.h"
+#include "DBDefines.h"
+#include "parameters.h"
 
   typedef struct DB{
       char *name;
@@ -14,4 +16,13 @@
   DB* createDB(void);
   void deleteDB(DB*);
   void prompt(DB*);
+  void error(void);
+  void newDB(reff(DB*),Parameters*);
+  void saveDB(DB*);
+  void listDB(DB*);
+  void activeDB(reff(DB*),Parameters*);
+  void newcab(DB*,Parameters*);
+  void listcab(DB*);
+  void activecab(DB*,Parameters*);
+  void setkv(DB*,Parameters*);
 #endif
